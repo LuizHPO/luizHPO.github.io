bc67fb96cba6460a1b9148f3ca75f3dd8a2f4fab
Luiz de Paula <luizh_@live.com>
2018-03-01 20:40:15 -0300

    2.0 fixed

---
+++ main.js
@@ -0,0 +1,38 @@
+//FULLPAGE
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		anchors:['firstPage', 'secondPage', 'thirdPage'], paddingBottom: '20px',navigation: true, menu: '.header'
+	});
+});
+
+//Typed
+var typed = new Typed('.text-typed', {
+  strings: ["Olá!<br>^800\nSeja bem vindo.<br>^800\nEu sou o Luiz."],
+  typeSpeed: 60, startDelay: 1000, showCursor: false
+});
+
+//Portfolio
+
+var tela = $(window).width();
+
+function portfolio() {
+
+   if (tela > 739) {
+    
+  $('.portfolio img, .portfolio-img figcaption').on('mouseenter', function(){
+  $('.portfolio-img figcaption').addClass('ativo');
+  $('.portfolio img').addClass('ativo');
+  $(this).on('mouseleave', function(){
+    $('.portfolio-img figcaption, .portfolio img').removeClass('ativo');
+  });
+});
+    
+} 
+  
+}
+
+portfolio();
+                    
+
+  
+
