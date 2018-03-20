6c76ffb2073b416121216215fb207cb25e917997
Luiz de Paula <luizh_@live.com>
2018-03-19 23:47:53 -0300

    totally new portf

--- main.js
+++ main.js
@@ -1,38 +1,27 @@
-//FULLPAGE
-$(document).ready(function() {
-	$('#fullpage').fullpage({
-		anchors:['firstPage', 'secondPage', 'thirdPage'], paddingBottom: '30px',navigation: true, menu: '.header'
-	});
+$('.home a').click(function(e){
+  e.preventDefault();
+  var id = $(this).attr('href'),
+      targetOffSet = $(id).offset().top;
+  
+  $('html, body').animate({
+    scrollTop: targetOffSet
+  }, 500);
 });
 
-//Typed
-var typed = new Typed('.text-typed', {
-  strings: ["Olá!<br>^800\nSeja bem vindo.<br>^800\nEu sou o Luiz."],
-  typeSpeed: 60, startDelay: 1000, showCursor: false
-});
-
-//Portfolio
-
-var tela = $(window).width();
-
-function portfolio() {
-
-   if (tela > 739) {
+$('section').each(function(){
+  var height = $(this).height(),
+      offSetTop = $(this).offset().top,
+      id = $(this).attr('id'),
+      itemMenu = $('a[href="#' + id +'"]'),
+      menuHeight = $('.header').innerHeight();
+  
+  $(window).scroll(function(){
+    var scrollTop = $(window).scrollTop();
     
-  $('.portfolio img, .portfolio-img figcaption').on('mouseenter', function(){
-  $('.portfolio-img figcaption').addClass('ativo');
-  $('.portfolio img').addClass('ativo');
-  $(this).on('mouseleave', function(){
-    $('.portfolio-img figcaption, .portfolio img').removeClass('ativo');
+    if (offSetTop - menuHeight < scrollTop && offSetTop + height - menuHeight > scrollTop) {
+      itemMenu.addClass('ativo');
+    } else {
+      itemMenu.removeClass('ativo');
+    }
   });
 });
-    
-} 
-  
-}
-
-portfolio();
-                    
-
-  
-
