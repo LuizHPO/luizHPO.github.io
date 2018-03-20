4deff2ee0a4b2bd66cdcd30f401ffc9bbfa27a76
Luiz de Paula <luizh_@live.com>
2018-03-20 16:34:45 -0300

    design update
    some changes

--- main.js
+++ main.js
@@ -8,20 +8,4 @@ $('.home a').click(function(e){
   }, 500);
 });
 
-$('section').each(function(){
-  var height = $(this).height(),
-      offSetTop = $(this).offset().top,
-      id = $(this).attr('id'),
-      itemMenu = $('a[href="#' + id +'"]'),
-      menuHeight = $('.header').innerHeight();
-  
-  $(window).scroll(function(){
-    var scrollTop = $(window).scrollTop();
-    
-    if (offSetTop - menuHeight < scrollTop && offSetTop + height - menuHeight > scrollTop) {
-      itemMenu.addClass('ativo');
-    } else {
-      itemMenu.removeClass('ativo');
-    }
-  });
-});
+
